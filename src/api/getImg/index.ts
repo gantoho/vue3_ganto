c126a8fe1ff84e74f36208cfbd3975f595219ea7
干徒 <i@ganto.icu>
2023-12-29 23:51:40 +0800

    chore: commitlint插件配置

--- src/api/getImg/index.ts
+++ src/api/getImg/index.ts
@@ -2,8 +2,8 @@ import { getServerImgHttp } from "@/utils/request";
 import type { axiosRequestConfig } from './type'
 
 enum API {
-  GET_IMG = '/bgimg',
+  GET_IMG  =  '/bgimg',
 }
 
 // 获取列表数据接口
-export const getImg = (config: axiosRequestConfig) => getServerImgHttp({...config, url: API.GET_IMG})
+export const getImg = (config: axiosRequestConfig) => getServerImgHttp({ ...config, url: API.GET_IMG })
