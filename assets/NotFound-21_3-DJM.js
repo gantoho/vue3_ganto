a6b1a29dcf2c7c2ded5e1109c263fe4e621a5195
gantoho <gantoho@users.noreply.github.com>
2024-06-08 00:42:07 +0000

    Deploying to gh-pages from @ gantoho/vue3_ganto@1a94bda4a970ad776727f061039b37eab98c8b64 🚀

--- assets/NotFound-21_3-DJM.js
+++
@@ -1,1 +0,0 @@
-import{h as e,l as t,k as o}from"./index-B50Kk87Z.js";const n={class:"not_found text-red-600"},r=e({__name:"NotFound",setup(s){return(_,a)=>(o(),t("div",n," 😓 你访问的页面不存在～ "))}});export{r as default};
